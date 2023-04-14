ac92684555e26868869fb6884dcea876c9f0fc98
Moonxuh <125825451+Moonxuh@users.noreply.github.com>
2023-04-14 17:23:24 -0300

    Finalização
    Finalizado o Projeto CCXP22.

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const dia = document.getElementById('dia')
-const hora = document.getElementById('hora')
-const minuto = document.getElementById('minuto')
-const segundo = document.getElementById('segundo')
+const dia = document.getElementById("dia")
+const hora = document.getElementById("hora")
+const minuto = document.getElementById("minuto")
+const segundo = document.getElementById("segundo")
 
-const lancamento = "14 april 2023"
+const lancamento = "2023/04/14"
 
-function countDown(){
-    const dataLanc = new Date(lancamento)
-    const hoje = new Date()
+function countDown() {
+  const dataLanc = new Date(lancamento)
+  const hoje = new Date()
 
-    const segTotal = (dataLanc - hoje)/1000;
+  const segTotal = (dataLanc - hoje) / 1000
 
-    const finalDias = Math.floor (segTotal / 60 / 60 / 24);
-    const finalHoras = Math.floor (segTotal / 60 / 60)% 24;
-    const finalMinutos = Math.floor (segTotal / 60)% 60;
-    const finalSegundos = Math.floor (segTotal)% 60;
+  const finalDias = Math.floor(segTotal / 60 / 60 / 24)
+  const finalHoras = Math.floor(segTotal / 60 / 60) % 24
+  const finalMinutos = Math.floor(segTotal / 60) % 60
+  const finalSegundos = Math.floor(segTotal) % 60
 
-    dia.innerHTML = `${formatoTempo(finalDias)}D`
-    hora.innerHTML = `${formatoTempo (finalHoras)}H`
-    minuto.innerHTML = `${formatoTempo (finalMinutos)}M`
-    segundo.innerHTML = `${formatoTempo (finalSegundos)}S`
-
+  dia.innerHTML = `${formatoTempo(finalDias)}D`
+  hora.innerHTML = `${formatoTempo(finalHoras)}H`
+  minuto.innerHTML = `${formatoTempo(finalMinutos)}M`
+  segundo.innerHTML = `${formatoTempo(finalSegundos)}S`
 }
-function formatoTempo ( tempo ){
-    return tempo < 10? `${tempo}` : tempo;
+function formatoTempo(tempo) {
+  return tempo < 10 ? `${tempo}` : tempo
 }
-countDown();
+countDown()
 console.log("Abacaxi")
 setInterval(countDown, 1000)
